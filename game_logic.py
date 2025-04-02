66d3976c52639700efb8e7b525c306fc3925e965
Anenokil <anenokil.w@gmail.com>
2025-04-03 01:56:39 +0300

    Define playing cards
    Add drafts of card classes

--- game_logic.py
+++ game_logic.py
@@ -1,3 +1,6 @@
+import abc
+import enum
+
 # Typing
 Hours = int
 Points = int
@@ -28,3 +31,65 @@ class Task:
         self.penalty = penalty
         self.event_on_success = event_on_success
         self.event_on_fail = event_on_fail
+
+
+class Card(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self, name: str, description: str, image: Image):
+        """
+        Playing card
+
+        :param name: Card name
+        :param description: Card description
+        :param image: Card image
+        """
+        self.name = name
+        self.description = description
+        self.image = image
+
+
+class ActionCard(Card):
+    def __init__(self, name: str, description: str, image: Image, cost: Hours,
+                 action: callable, req_args: list[str], check_args: callable):
+        """
+        Action card - one of the card types
+
+        :param name: Card name
+        :param description: Card description
+        :param image: Card image
+        :param cost: The cost of the card in hours
+        :param action: Card action
+        :param req_args: A list of parameter names that the `action` function accepts
+        :param check_args: A function for verifying the correctness of parameters
+            passed to the `action` function
+        """
+        super().__init__(name, description, image)
+        self.cost = cost
+        self.action = action
+        self.req_args = req_args
+        self.check_args = check_args
+
+
+class TaskTarget(enum.Enum):
+    """
+    Targets for cards
+    """
+    ME = 1  # Player
+    OPPONENT = 2  # Opponent
+    BOTH = 3  # Player or his opponent
+
+
+class TaskCard(Card):
+    def __init__(self, name: str, description: str, image: Image, task: Task, valid_targets: TaskTarget):
+        """
+        Task card - one of the card types
+
+        :param name: Card name
+        :param description: Card description
+        :param image: Card image
+        :param task: A task that is given when the card is played
+        :param valid_targets: Valid targets
+        """
+        super().__init__(name, description, image)
+        self.task = task
+        self.valid_targets = valid_targets
